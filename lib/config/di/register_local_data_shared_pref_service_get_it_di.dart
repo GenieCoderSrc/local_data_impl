4a598b309676ac5ed80d2a2db24d8ac5fa2dc166
SuraiyaKhatunItHome <suraiya.it.home@gmail.com>
2025-05-25 01:12:00 +0600

    registerLocalHiveDataSourceServiceGetItDI

--- lib/config/di/register_local_data_shared_pref_service_get_it_di.dart
+++ lib/config/di/register_local_data_shared_pref_service_get_it_di.dart
@@ -6,7 +6,7 @@ import 'package:local_data_impl/data/data_sources/local_data_service_hydrated_im
 import 'package:local_data_impl/data/data_sources/memory_storage_service_shared_prefs_impl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void localDataServiceGetItRegister() async {
+void registerLocalDataSharedPrefServiceGetItDi() async {
   /// ----------------- Local Data--------------
   //! External
   final SharedPreferences sharedPreferences =
